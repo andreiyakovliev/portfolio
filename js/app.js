417ece64380ceae6c7e3b936066bc4576bf5ad3c
Andrii Yakovliev <andrii.iakovliev@gmail.com>
2024-11-15 12:35:51 +0200

    Fixed bug in src of qr-code image

--- js/app.js
+++ js/app.js
@@ -4757,7 +4757,7 @@
             }
         }
         vcardHrefs.forEach((vcardHref => {
-            vcardHref.href = `data/data/contact.${lang}.vcf`;
+            vcardHref.href = `data/contact.${lang}.vcf`;
         }));
     }
     document.querySelectorAll(".language-mode__button a").forEach((link => {
